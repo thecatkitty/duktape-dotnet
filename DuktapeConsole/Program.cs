b33892c358eda9e81aa59524be437794d751fb2a
Mateusz Karcz <mateusz.karcz@interia.eu>
2018-03-02 23:32:26 +0100

    DuktapeTest -> DuktapeConsole

---
+++ DuktapeConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuktapeConsole {
+  class Program {
+
+    static void Main(string[] args) {
+      Console.Title = "Celones Duktape.NET Console, version "
+        + System.Reflection.Assembly.GetExecutingAssembly().GetName()
+        .Version.ToString();
+
+      using(var js = new Duktape.Context()) {
+        var line = "";
+        while(true) {
+          Console.Write("> ");
+          line = Console.ReadLine();
+
+          if(line == "exit")
+            break;
+
+          try {
+            var value = js.Run(line);
+            Console.WriteLine(value.ToString());
+          } catch(Exception e) {
+            Console.Error.WriteLine(e.GetType().Name + ": " + e.Message);
+          }
+        }
+      }
+    }
+
+  }
+}
--- DuktapeConsole/Properties/AssemblyInfo.cs
+++ DuktapeConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("DuktapeTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Duktape.NET Console")]
+[assembly: AssemblyDescription("ECMAScript console based on Duktape.NET library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DuktapeTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Celones")]
+[assembly: AssemblyProduct("Duktape.NET")]
+[assembly: AssemblyCopyright("© 2018 Mateusz Karcz. Licensed according to the MIT License.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
